bbd9f6b30197ec81f8427db63c2ec737575d8f25
Klajdi Caushi <klajdi.caushi@cardoai.com>
2022-01-18 00:08:43 +0100

    fix(entries): removed entry content

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -11,7 +11,6 @@ import CardActionArea from '@mui/material/CardActionArea';
 import Typography from '@mui/material/Typography';
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import TypographyWithMaxRows from "./reusable/TypographyWithMaxRows";
 import MoodPicker from "./reusable/MoodPicker";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
@@ -116,7 +115,7 @@ const Journal = () => {
           <Grid item key={entry.id} xs={12} lg={6} xl={3}>
             <Card>
               <CardActionArea onClick={openEntry(entry.id)}>
-                <CardContent sx={{height: 211}}>
+                <CardContent>
                   <Typography variant="h6">
                     {entry.title || "No title"}
                   </Typography>
@@ -130,9 +129,6 @@ const Journal = () => {
                       <MoodPicker readOnly value={entry.rating}/>
                     </Grid>
                   </Grid>
-                  <TypographyWithMaxRows variant="body2" rows={5}>
-                    {entry.content}
-                  </TypographyWithMaxRows>
                 </CardContent>
               </CardActionArea>
             </Card>
