e948bccc0f30745b693922789671c90c4d448da8
Klajdi Çaushi <klajdi.caushi@cardoai.com>
2022-07-18 22:55:40 +0200

    build: configurations for heroku

--- backend/lajournal/settings/dev.py
+++ backend/lajournal/settings/dev.py
@@ -2,4 +2,11 @@ from .defaults import *  # noqa
 
 DEBUG = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 ALLOWED_HOSTS = []
--- backend/lajournal/settings/prod.py
+++ backend/lajournal/settings/prod.py
@@ -1,4 +1,31 @@
 from .defaults import *  # noqa
 
+import dj_database_url
+
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
